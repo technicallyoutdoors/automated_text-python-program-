af3b3ca33df60d7fc3601cf40593e0323f788a6f
Matt <mattmill1217@yahoo.com>
2023-01-24 22:06:30 -0600

    fixed the class method issue and created an object for the __init__ method

--- automated_text.py
+++ automated_text.py
@@ -2,7 +2,7 @@ import schedule
 import time
 from phone_number import phone_number
 import json, requests
-
+#must use a valid API key here
 api_key = "REDACTED"
 city = "Houston"
 url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+api_key+"&units=imperial"
@@ -19,74 +19,79 @@ humidity = json.get("main").get("humidity")
 wind_speed = json.get("wind").get("speed")
 
 
-class text_message:
+class Text_message:
     def __init__(self, temperature, humidity):
         self.__init__
         self.temperature = temperature
         self.humidity = humidity
         self.phone_number = phone_number
 
-    def send_message_monday():
+t1 = Text_message("85 degrees", "75percent")
 
-        resp = request.post('https://textbelt.com/text', {
-            'phone': phone_number,
-            'message': 'Good evening, please vacuum the entire house and conduct the daily evening cleaning tasks',
-            'key': 'textbelt',
-        })
-        print(resp.json())
+print(t1.temperature)
+print(t1.humidity)
 
-    def send_message_tuesday():
+def send_message_monday():
 
-        resp = request.post('https://textbelt.com/text', {
-            'phone': phone_number,
-            'message': 'Clean all the bathrooms, master, guest and spare upstairs',
-            'key': 'textbelt',
-        })
-        print(resp.json())
+    resp = request.post('https://textbelt.com/text', {
+        'phone': phone_number,
+        'message': 'Good evening, please vacuum the entire house and conduct the daily evening cleaning tasks',
+        'key': 'textbelt',
+    })
+    print(resp.json())
 
-    def send_message_wednesday():
+def send_message_tuesday():
 
-        resp = request.post('https://textbelt.com/text', {
-            'phone': phone_number,
-            'message': 'Dust everything - fans, mantel, tvs, etc.',
-            'key': 'textbelt',
-        })
-        print(resp.json())
+    resp = request.post('https://textbelt.com/text', {
+        'phone': phone_number,
+        'message': 'Clean all the bathrooms, master, guest and spare upstairs',
+        'key': 'textbelt',
+    })
+    print(resp.json())
 
-    def send_message_thursday():
+def send_message_wednesday():
 
-        resp = request.post('https://textbelt.com/text', {
-            'phone': phone_number,
-            'message': 'Mop all floors including bathroom areas',
-            'key': 'textbelt',
-        })
-        print(resp.json())
+    resp = request.post('https://textbelt.com/text', {
+        'phone': phone_number,
+        'message': 'Dust everything - fans, mantel, tvs, etc.',
+        'key': 'textbelt',
+    })
+    print(resp.json())
 
-    def send_message_friday():
+def send_message_thursday():
 
-        resp = request.post('https://textbelt.com/text', {
-            'phone': phone_number,
-            'message': 'Monthly chore, 1st = Wipe basboards and cabinets, 2nd = Clean stove and microwave, 3rd = Clean out fridge and freezer (garage and indoor) 4th = Clean windows inside and out',
-            'key': 'textbelt',
-        })
-        print(resp.json())
+    resp = request.post('https://textbelt.com/text', {
+        'phone': phone_number,
+        'message': 'Mop all floors including bathroom areas',
+        'key': 'textbelt',
+    })
+    print(resp.json())
 
-    def send_message_saturday():
+def send_message_friday():
 
-        resp = request.post('https://textbelt.com/text', {
-            'phone': phone_number,
-            'message': 'Conduct the laundry for the week and complete any project you have been wanting to get done',
-            'key': 'textbelt',
-        })
-        print(resp.json())
+    resp = request.post('https://textbelt.com/text', {
+        'phone': phone_number,
+        'message': 'Monthly chore, 1st = Wipe basboards and cabinets, 2nd = Clean stove and microwave, 3rd = Clean out fridge and freezer (garage and indoor) 4th = Clean windows inside and out',
+        'key': 'textbelt',
+    })
+    print(resp.json())
+
+def send_message_saturday():
+
+    resp = request.post('https://textbelt.com/text', {
+        'phone': phone_number,
+        'message': 'Conduct the laundry for the week and complete any project you have been wanting to get done',
+        'key': 'textbelt',
+    })
+    print(resp.json())
 
 
-        if temp_max > 80:
-            print("it's way too hot, stay inside")
-        elif humidity > 65:
-            print("Do not go outside, it's so humid it's bascailly raining lol")
-        elif temp_max < 78:
-            print("it's actually pretty nice out")
+    if temp_max > 80:
+        print("it's way too hot, stay inside")
+    elif humidity > 65:
+        print("Do not go outside, it's so humid it's bascailly raining lol")
+    elif temp_max < 78:
+        print("it's actually pretty nice out")
 
 
     schedule.every().monday.at("19:00").do(send_message_monday())
